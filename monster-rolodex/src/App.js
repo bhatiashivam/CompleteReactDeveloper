6497fbf88ed33c571d713b85ace6de65112de8bb
Kevin Pham <phambamkevin@gmail.com>
2020-10-22 23:04:40 -0700

    Used a LifeCycleMethod called componentDidMount

--- monster-rolodex/src/App.js
+++ monster-rolodex/src/App.js
@@ -1,18 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  // Fetch from the URL the response and converting to a JSON format then take the users and set our monsters to that array
+  // componentDidMount is a LifeCycleMethod that calls whatever code is within when React renders the component in the DOM for the first time
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}> {monster.name} </h1>
+        ))}
       </div>
     );
   }
