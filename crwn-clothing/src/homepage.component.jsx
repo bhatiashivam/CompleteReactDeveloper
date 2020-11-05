86f26c2e0cd806c85ebd47c14356f7624e062c24
Kevin Pham <phambamkevin@gmail.com>
2020-11-05 13:38:03 -0800

    Fixed error with SASS

--- crwn-clothing/src/homepage.component.jsx
+++ crwn-clothing/src/homepage.component.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "./homepage.styles.scss";
+
 const HomePage = () => (
   <div className="homepage">
     <div className="directory-menu">
@@ -9,32 +11,27 @@ const HomePage = () => (
           <span className="subtitle">SHOP NOW</span>
         </div>
       </div>
-    </div>
-    <div className="directory-menu">
       <div className="menu-item">
         <div className="content">
           <h1 className="title">JACKETS</h1>
           <span className="subtitle">SHOP NOW</span>
         </div>
       </div>
-    </div>
-    <div className="directory-menu">
+
       <div className="menu-item">
         <div className="content">
           <h1 className="title">SNEAKERS</h1>
           <span className="subtitle">SHOP NOW</span>
         </div>
       </div>
-    </div>
-    <div className="directory-menu">
+
       <div className="menu-item">
         <div className="content">
           <h1 className="title">WOMENS</h1>
           <span className="subtitle">SHOP NOW</span>
         </div>
       </div>
-    </div>
-    <div className="directory-menu">
+
       <div className="menu-item">
         <div className="content">
           <h1 className="title">MENS</h1>
